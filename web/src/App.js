c964477590a73345d8e67d74473f3a25f771e0a8
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-06-14 11:17:09 -0300

    implementação visual e funcional básicas da tela de login

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 import Routes from './routes';
 
 const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color:#747474;
   margin: 0;
   width: 100vw;
   height: 100vh;
